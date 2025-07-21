2665240abf3ae096c9e408597002230a0f3e6d43
mrrossi76 <44334798+mrrossi76@users.noreply.github.com>
2025-07-21 14:02:45 +0200

    Create FoodItemAnalysis.swift

---
+++ Models/FoodItemAnalysis.swift
@@ -0,0 +1,8 @@
+struct FoodItemAnalysis {
+    let name: String
+    let quantity: String
+    let carbs: Double
+    let calories: Double?
+    let preparationMethod: String?
+    let confidence: String?
+}
